0e690a6411cd623b6fd4bb741645bb00c06b959b
SamudraWijayaS <swijayasamdoria@gmail.com>
2026-02-14 12:59:02 +0700

    delete migration

--- prisma/migrations/20260212070027_add_cascade_kegiatan_dokumentasi/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE `kegiatandokumentasi` DROP FOREIGN KEY `KegiatanDokumentasi_kegiatanId_fkey`;
-
--- DropIndex
-DROP INDEX `KegiatanDokumentasi_kegiatanId_fkey` ON `kegiatandokumentasi`;
-
--- AddForeignKey
-ALTER TABLE `KegiatanDokumentasi` ADD CONSTRAINT `KegiatanDokumentasi_kegiatanId_fkey` FOREIGN KEY (`kegiatanId`) REFERENCES `Kegiatan`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
--- prisma/migrations/20260213010143_add_setting/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE `Setting` (
-    `id` INTEGER NOT NULL DEFAULT 1,
-    `maintenanceMode` BOOLEAN NOT NULL DEFAULT false,
-    `maintenanceMsg` VARCHAR(191) NULL,
-    `updatedAt` DATETIME(3) NOT NULL,
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
